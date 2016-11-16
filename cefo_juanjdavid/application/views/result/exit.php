6a9b1476a157685a1f4fce767aaf2d94324589a1
Haritakari <davidsanchezdevesa@gmail.com>
2016-11-16 18:55:38 +0100

    1.0 falta netejar al guna cosa (login funciona 99999999Z 1970-01-01)

--- cefo_juanjdavid/application/views/result/exit.php
+++ cefo_juanjdavid/application/views/result/exit.php
@@ -1,1 +1,11 @@
-<?php
+
+	<section class="content">
+	
+	      <div class="container">
+		<h1><?php echo $heading; ?></h1>
+		<?php echo $message; ?>
+			</div>
+		
+	</section>
+
+<?php Templ::footer(); ?>    
